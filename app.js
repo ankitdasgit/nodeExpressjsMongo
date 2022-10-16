76d7cf09b08bd9b8a8f83efa90873c10c67d1b7a
ankit das <ankit.das2019@sait.ac.in>
2022-10-16 18:31:33 +0530

    basic node operation

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
-const http = require('http');
+// const Circle= require('./circle');
 
+// const circle = new Circle();
+// console.log(circle.area(2));
+// console.log(circle.circumference(3)); 
+
+// console.log(__filename);
+// console.log(__dirname);
+
+// console.log(circle.area(2), circle.circumference(3));
+
+
+// const {area, circumference} = require('./circle');
+// console.log(area(2),circumference(3));
+
+
+
+const http = require('http');
+const port =4000;
 const server = http.createServer((req, res)=>{
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify({
-      data: 'Hello World!'
+      data: 'Hello World!',
     }));
 });
-
-server.listen(3000); 
+console.log(`server is running at http://localhost:${port}`);
+server.listen(port); 
